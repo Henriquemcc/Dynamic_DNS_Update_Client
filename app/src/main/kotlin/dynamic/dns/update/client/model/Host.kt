6f779bb8a08fb35f671c61074793a1768effad17
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-10-30 12:15:34 -0300

    Adding Host class.

---
+++ app/src/main/kotlin/dynamic/dns/update/client/model/Host.kt
@@ -0,0 +1,24 @@
+package dynamic.dns.update.client.model
+
+import java.io.Serializable
+import java.time.Duration
+
+abstract class Host(val hostname: String = "", val enableIPv4: Boolean = true, val enableIPv6: Boolean = true, val updateDelayTime: Duration) : Serializable {
+
+    override fun toString(): String {
+        val stringBuilder = StringBuilder()
+        stringBuilder.appendLine(super.toString())
+        stringBuilder.appendLine("hostname = $hostname")
+        stringBuilder.appendLine("enableIPv4 = $enableIPv4")
+        stringBuilder.appendLine("enableIPv6 = $enableIPv6")
+        return stringBuilder.toString()
+    }
+
+    fun sleep() {
+        Thread.sleep(updateDelayTime.toMillis())
+    }
+
+    abstract fun performUpdate(looping: Boolean = false)
+
+    abstract fun performCleaning()
+}

f22cd0100226d3d458d72bf77f8b1c197cd0bc73
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-10-31 10:06:13 -0300

    Removing useless parallelism in App.kt.

--- app/src/main/kotlin/dynamic/dns/update/client/App.kt
+++ app/src/main/kotlin/dynamic/dns/update/client/App.kt
@@ -5,19 +5,8 @@ import dynamic.dns.update.client.graphic.MainGraphicMenu
 
 fun main() {
 
-    val thread1 = object : Thread() {
-        override fun run() {
-            MainGraphicMenu()
-        }
-    }
+    MainGraphicMenu()
 
+    MainConsoleMenu()
 
-    val thread2 = object : Thread() {
-        override fun run() {
-            MainConsoleMenu()
-        }
-    }
-
-    thread1.start()
-    thread2.start()
 }

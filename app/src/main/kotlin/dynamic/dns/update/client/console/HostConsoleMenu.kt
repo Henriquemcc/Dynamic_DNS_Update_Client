636ad67d01634767ffb80fc34e2e1f22a6638bdf
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-10-31 09:16:35 -0300

    Creating Duck DNS host main console menu.

--- app/src/main/kotlin/dynamic/dns/update/client/console/HostConsoleMenu.kt
+++ app/src/main/kotlin/dynamic/dns/update/client/console/HostConsoleMenu.kt
@@ -2,9 +2,10 @@ package dynamic.dns.update.client.console
 
 import dynamic.dns.update.client.console.common.menu.ConsoleButton
 import dynamic.dns.update.client.console.common.menu.ConsoleMenu
+import dynamic.dns.update.client.console.duckdns.DuckDnsHostMainConsoleMenu
 
 class HostConsoleMenu : ConsoleMenu("Host menu", listOf(
     ConsoleButton("Duck DNS subdomain") {
-
+        DuckDnsHostMainConsoleMenu()
     }
 ))
---
+++ app/src/main/kotlin/dynamic/dns/update/client/console/duckdns/DuckDnsHostMainConsoleMenu.kt
@@ -0,0 +1,20 @@
+package dynamic.dns.update.client.console.duckdns
+
+import dynamic.dns.update.client.console.common.menu.ConsoleButton
+import dynamic.dns.update.client.console.common.menu.ConsoleMenu
+
+class DuckDnsHostMainConsoleMenu : ConsoleMenu("Duck DNS subdomain menu", listOf(
+    ConsoleButton("Show subdomains") {
+
+    },
+    ConsoleButton("Add a subdomain") {
+
+    },
+    ConsoleButton("Modify a subdomain") {
+
+    },
+    ConsoleButton("Remove a subdomain") {
+
+    }
+
+))

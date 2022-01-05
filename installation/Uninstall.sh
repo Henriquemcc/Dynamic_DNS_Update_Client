c9e79fd75df615959cd910d5a8fb6ea246631448
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-01-05 10:48:09 -0300

    Adding uninstall script

---
+++ installation/Uninstall.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+repository_zip_url="https://github.com/Henriquemcc/Dynamic_DNS_Update_Client/archive/refs/heads/main.zip"
+destination_directory="/tmp"
+
+function download_repository()
+{
+  # Creating destination directory
+  if ! [ -d $destination_directory ]; then
+      sudo mkdir -p $destination_directory
+  fi
+
+  # Defining destination file path
+  destination_file_path="${destination_directory}/Dynamic_DNS_Update_Client.zip"
+
+  # Downloading file
+  curl -L $repository_zip_url --output $destination_file_path
+
+  # Returning destination file path
+  echo $destination_file_path
+}
+
+
+function uninstall_on_Linux()
+{
+  # Downloading repository
+  file_path="$(download_repository)"
+
+  # Unzipping file
+  unzip "$file_path" || exit 1
+
+  # Storing path
+  old_path="$(pwd)"
+
+  # Changing directory
+  cd "${destination_directory}/Dynamic_DNS_Update_Client-main/installation/Linux" || exit 1
+
+  # Installing
+  bash ./Uninstall.sh
+
+  # Returning to the old path
+  cd "$old_path" || exit 1
+}
+
+if [ "$(uname)" == "Linux" ]; then
+  uninstall_on_Linux
+fi

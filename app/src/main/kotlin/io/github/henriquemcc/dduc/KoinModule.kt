4def66eb39b3772538451cc683b4eb141fe6c2f3
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-03-05 08:04:09 -0300

    Fixing dependency injection

--- app/src/main/kotlin/io/github/henriquemcc/dduc/KoinModule.kt
+++ app/src/main/kotlin/io/github/henriquemcc/dduc/KoinModule.kt
@@ -10,12 +10,22 @@ import io.github.henriquemcc.dduc.service.NetworkService
 import io.github.henriquemcc.dduc.service.NetworkServiceImpl
 import io.github.henriquemcc.dduc.provider.DynamicDnsProvider
 import io.github.henriquemcc.dduc.provider.DuckDnsProvider
+import org.koin.core.module.dsl.bind
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val koinModule = module {
     single<DynamicDnsRepository> { DynamicDnsRepositoryImpl() }
     single<NetworkService> { NetworkServiceImpl() }
-    factory<DynamicDnsProvider> { DuckDnsProvider() } // Register GenericDynamicDnsProvider
-    factory<DynamicDnsCli> { DuckDnsCli(get(), get()) }
+    single<DynamicDnsProvider> { DuckDnsProvider() } // Register GenericDynamicDnsProvider
+
+    // Register individual DynamicDnsCli implementations
+    singleOf(::DuckDnsCli) { bind<DynamicDnsCli>() }
+    // If you had other implementations, you'd add them here:
+    // singleOf(::AnotherDnsCli) { bind<DynamicDnsCli>() }
+
+    // Explicitly provide the List<DynamicDnsCli> by collecting all registered DynamicDnsCli
+    single<List<DynamicDnsCli>> { getAll() }
+
     single<DynamicDnsService> { DynamicDnsServiceImpl(getAll(), get()) } // Inject all DynamicDnsProviders
 }

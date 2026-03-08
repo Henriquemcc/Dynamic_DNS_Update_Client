5673083d68cb2d7f238dc61cd87619b3b0c9ccf6
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-03-07 22:50:57 -0300

    Sending the version of the tag to the generation of the MSI installer

--- installer/Build.ps1
+++ installer/Build.ps1
@@ -1,5 +1,10 @@
-# Installing DotNet SDK
-winget install Microsoft.DotNet.SDK.10
+param(
+    # Version sent by GitHub Actions workflow.
+    [string]$Version = "0.0.0"
+)
+
+# Removes the prefix 'v' from the tag name (eg: v1.2.3 -> 1.2.3)
+$ProductVersion = $Version.TrimStart('v')
 
 # Building MSI Package
-dotnet build
+dotnet build /p:ProductVersion=$ProductVersion

e6ff1c7714f97ce7fce1866c0c105623a6061869
Henrique Mendonça Castelar Campos <henriquemcc@protonmail.ch>
2026-03-15 08:10:35 -0300

    Adding the download of WinSw in the PowerShell script

--- installer/Build.ps1
+++ installer/Build.ps1
@@ -6,5 +6,26 @@ param(
 # Removes the prefix 'v' from the tag name (eg: v1.2.3 -> 1.2.3)
 $ProductVersion = $Version.TrimStart('v')
 
+# Downloading WinSw
+$winSwVersion = "v2.12.0"
+$downloadDir = [System.IO.Path]::Combine([System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Definition), "winsw-download")
+$downloads = @{
+    "WinSW-x64.exe" = "https://github.com/winsw/winsw/releases/download/$winSwVersion/WinSW-x64.exe"
+    "WinSW-x86.exe" = "https://github.com/winsw/winsw/releases/download/$winSwVersion/WinSW-x86.exe"
+}
+New-Item -Path $downloadDir -ItemType Directory -Force
+foreach($download in $downloads.GetEnumerator()) {
+    $fileName = $download.Name
+    $urlString = $download.Value
+
+    $targetFile = [System.IO.Path]::Combine($downloadDir, $fileName)
+    if (-not ([System.IO.File]::Exists($targetFile))){
+        Write-Output -InputObject "Downloading $fileName from $urlString..."
+        Invoke-WebRequest -Uri $urlString -OutFile $targetFile
+    } else {
+        Write-Output -InputObject "$fileName already exists. Skipping download."
+    }
+}
+
 # Building MSI Package
 dotnet build /p:ProductVersion=$ProductVersion /p:DefineConstants="ProductVersion=$ProductVersion"
